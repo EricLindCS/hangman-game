c9150d7cc47146c1d1271e272e62213a36a1046f
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2024-12-21 22:36:28 -0800

    Fixed Words

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def guess():
     if letter in string.ascii_uppercase and letter not in session['used_letters']:
         session['used_letters'].append(letter)
         if letter in session['word_letters']:
-            session['word_letters'].remove(letter)
+            session['word_letters'] = [l for l in session['word_letters'] if l != letter]
             session['output'].append(f">>> {letter}\nCorrect!")
         else:
             session['lives'] -= 1
@@ -89,18 +89,28 @@ def guess():
     else:
         session['output'].append(f">>> {letter}\nInvalid input or already used letter.")
     
-    session['hangman_visual'] = HANGMAN_VISUAL[6 - session['lives']]
+    session['hangman_visual'] = HANGMAN_VISUAL[7 - session['lives']]
     
     if session['lives'] == 0:
         session['output'].append("Game Over! You've run out of lives.")
         session['game_over'] = True
-        return jsonify(game_state=session), 200
     elif len(session['word_letters']) == 0:
         session['output'].append("Congratulations! You've guessed the word.")
         session['output'].append("walnutshrimp.aternos.me")
-        return jsonify(game_state=session), 200
     
-    return jsonify(game_state=session), 200
+    print(len(session['word_letters']))
+
+    game_state = {
+        'word': session['word'],
+        'word_letters': session['word_letters'],
+        'used_letters': session['used_letters'],
+        'lives': session['lives'],
+        'output': session['output'],
+        'hangman_visual': session['hangman_visual'],
+        'game_over': session['game_over']
+    }
+    
+    return jsonify(game_state=game_state), 200
 
 @app.route('/game_over')
 def game_over():
@@ -114,9 +124,9 @@ def index():
         session['word'] = random.choice(words).upper()
         session['word_letters'] = list(session['word'])
         session['used_letters'] = []
-        session['lives'] = 6
+        session['lives'] = 7
         session['output'] = []
-        session['hangman_visual'] = HANGMAN_VISUAL[6]
+        session['hangman_visual'] = HANGMAN_VISUAL[0]
         session['game_over'] = False
     return render_template('index.html', game_state=session)
 
@@ -132,4 +142,4 @@ def reset_game():
     return jsonify(message="Game has been reset"), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)  # Ensure this is set correctly
